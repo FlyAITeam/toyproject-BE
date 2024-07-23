095b2aa61e0bdca022278191bf99751c4548260e
SeungminKimdev <ksm7250432@gmail.com>
2024-07-23 10:48:14 +0900

    rename : db,회원조회 이름수정 #14

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/postgres"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- routers/auth.py
+++ routers/auth.py
@@ -103,16 +103,16 @@ async def signin(request: Request, db: Session = Depends(get_db)):
         )
         
 # 아이디 중복 확인
-@router.get("/check-userid", status_code=status.HTTP_200_OK)
-async def check_userid(userid: str = Query(default=None), db: Session = Depends(get_db)):
-    if not userid:
+@router.get("/check-loginid", status_code=status.HTTP_200_OK)
+async def check_loginid(loginid: str = Query(default=None), db: Session = Depends(get_db)):
+    if not loginid:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"errorMessage": "Userid cannot be null or empty."}
         )
 
     try:
-        db_user = get_user_by_loginId(db, userid)
+        db_user = get_user_by_loginId(db, loginid)
         if db_user:
             return JSONResponse(
                 status_code=status.HTTP_409_CONFLICT,
